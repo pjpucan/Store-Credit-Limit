106f15c584b45c72fcd4c59e6599b47f23c657f3
Patrick Pucan <patrickp@walkingco.com>
2025-03-01 21:05:51 +0800

    Frontend Setup

---
+++ webpack.mix.js
@@ -0,0 +1,16 @@
+let mix = require('laravel-mix');
+require('laravel-mix-purgecss');
+
+// Main theme files
+// mix.js('src/js/customjs.js', 'assets/')
+//    .sass('src/scss/styles.scss', 'assets/')
+//    .options({
+//      processCssUrls: false,
+//      hmrOptions: {
+//        https: false,
+//        host: 'localhost',
+//        port: '8080'
+//      }
+//    });
+
+// Add here the components you want to compile separately
